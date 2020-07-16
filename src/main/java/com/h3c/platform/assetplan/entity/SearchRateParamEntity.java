b0326593ca7985c84522cd77cb6317d12581d479
lkf7579 <lkf7579@LKF7579V.h3c.huawei-3com.com>
2020-07-16 17:44:52 +0800

    实体提交

---
+++ src/main/java/com/h3c/platform/assetplan/entity/SearchRateParamEntity.java
@@ -0,0 +1,68 @@
+package com.h3c.platform.assetplan.entity;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import springfox.documentation.annotations.ApiIgnore;
+
+public class SearchRateParamEntity {
+	//当前页
+	private Integer pageNum;
+    //每页记录数
+    private Integer pageSize;
+
+    //申请时间
+    @DateTimeFormat(pattern ="yyyy-MM-dd HH:mm:ss")
+    private Date collectTime;
+    
+    //小类
+    private String assetCategory;
+    
+    //申请部门
+    private String deptCode;
+
+
+	public Integer getPageNum() {
+		return pageNum;
+	}
+
+	public void setPageNum(Integer pageNum) {
+		this.pageNum = pageNum;
+	}
+
+	public Integer getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public Date getCollectTime() {
+		return collectTime;
+	}
+
+	public void setCollectTime(Date collectTime) {
+		this.collectTime = collectTime;
+	}
+
+	public String getDeptCode() {
+		return deptCode;
+	}
+
+	public void setDeptCode(String deptCode) {
+		this.deptCode = deptCode;
+	}
+
+	public String getAssetCategory() {
+		return assetCategory;
+	}
+
+	public void setAssetCategory(String assetCategory) {
+		this.assetCategory = assetCategory;
+	}    
+}
