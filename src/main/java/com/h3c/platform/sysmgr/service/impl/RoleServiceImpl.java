37af2d9f4232911eff3d6759a7f33d85d4e75a1a
lkf7579 <lkf7579@LKF7579V.h3c.huawei-3com.com>
2020-08-03 10:03:43 +0800

    afsp接口参数修改

--- src/main/java/com/h3c/platform/sysmgr/service/impl/RoleServiceImpl.java
+++ src/main/java/com/h3c/platform/sysmgr/service/impl/RoleServiceImpl.java
@@ -41,7 +41,7 @@ public class RoleServiceImpl implements RoleService {
 		
 		Map<String,String> headers = new HashMap<String, String>();
 		headers.put("token", token);
-		String params="{\"applicationId\":\""+applicationId+"\",\"userId\":\""+(user!=null?user.getDomainAccount():"")+"\"}";	
+		String params="{\"applicationId\":\""+applicationId+"\",\"userCode\":\""+(user!=null?user.getDomainAccount():"")+"\"}";	
 		String result= HttpClientUtil.sendHttpPostJsonWithHeader(eosUrl+url_UserRole, params,headers);
 		
 		JSONObject jsonResult = json.parseObject(result);
@@ -53,23 +53,24 @@ public class RoleServiceImpl implements RoleService {
 	@Override
 	public boolean checkIsAdmin() throws Exception{
 		boolean isAdmin=false;
-		UserInfo user= userService.getUserByEmpCode(UserUtils.getCurrentUserId());
 		JSONObject json = new JSONObject();
 		
 		String token=afspTokenService.getEosToken();
 		
 		Map<String,String> headers = new HashMap<String, String>();
 		headers.put("token", token);
-		String params="{\"applicationId\":\""+applicationId+"\",\"userId\":\""+(user!=null?user.getDomainAccount():"")+"\"}";	
+		String params="{\"applicationId\":\""+applicationId+"\",\"userCode\":\""+(UserUtils.getCurrentDominAccount())+"\"}";	
 		String result= HttpClientUtil.sendHttpPostJsonWithHeader(eosUrl+url_UserRole, params,headers);
 		
 		JSONObject jsonResult = json.parseObject(result);
-		JSONArray jsonArray =jsonResult.getJSONArray("data");
-		
-		for(int i=0;i<jsonArray.size();i++) {
-			if("R_000001".equals(ObjToStrUtil.ReplaceNullValue(jsonArray.getString(i)))) {
-				isAdmin=true;
-				break;
+		if(jsonResult.getBoolean("flag")) {
+			JSONArray jsonArray =jsonResult.getJSONArray("data");
+			
+			for(int i=0;i<jsonArray.size();i++) {
+				if("R_000001".equals(ObjToStrUtil.ReplaceNullValue(jsonArray.getString(i)))) {
+					isAdmin=true;
+					break;
+				}
 			}
 		}
 		
